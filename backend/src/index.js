a76594119c8ff6ed146850509c2147bbf0916917
phuocnv1 <vinhphuoc1892@gmail.com>
2026-06-02 10:16:33 +0700

    fix cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(
       "http://localhost:3000",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:5174",
-      "https://quanlylophoc-98ys.onrender.com",
+      "https://quanlylophoc-1.onrender.com",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
